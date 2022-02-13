60c268628a5b6ef2ee65682577b0af6311930931
bloodyfork <mohtor.138055@gmail.com>
2022-02-14 02:14:13 +0330

    fixed place of BaseModel and BaseManager

--- online_shop/customer/models.py
+++ online_shop/customer/models.py
@@ -2,25 +2,3 @@ from django.db import models
 
 
 # Create your models here.
-class BaseManager(models.Manager):
-
-    def get_queryset(self):
-        return super().get_queryset().filter(is_deleted=False)
-
-    def archive(self):
-        return super().get_queryset()
-
-
-class BaseModel(models.Model):
-    is_deleted = models.BooleanField(default=False)
-    create_timestamp = models.DateTimeField(auto_now_add=True)
-    modify_timestamp = models.DateTimeField(auto_now=True)
-    delete_timestamp = models.DateTimeField(default=None, null=True, blank=True)
-
-    objects = BaseManager()
-
-    class Meta:
-        abstract = True
-
-    def logical_delete(self):
-        self.is_deleted = True
