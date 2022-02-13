a397fa8f1489f900ffabfdeeb91ccfaa93c0c4c3
bloodyfork <mohtor.138055@gmail.com>
2022-02-14 02:12:00 +0330

    created core BaseModel

--- online_shop/customer/models.py
+++ online_shop/customer/models.py
@@ -9,3 +9,18 @@ class BaseManager(models.Manager):
 
     def archive(self):
         return super().get_queryset()
+
+
+class BaseModel(models.Model):
+    is_deleted = models.BooleanField(default=False)
+    create_timestamp = models.DateTimeField(auto_now_add=True)
+    modify_timestamp = models.DateTimeField(auto_now=True)
+    delete_timestamp = models.DateTimeField(default=None, null=True, blank=True)
+
+    objects = BaseManager()
+
+    class Meta:
+        abstract = True
+
+    def logical_delete(self):
+        self.is_deleted = True
