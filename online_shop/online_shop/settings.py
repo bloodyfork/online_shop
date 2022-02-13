c829aed3aad8b3009f50efa3746422c3b2bc8188
bloodyfork <mohtor.138055@gmail.com>
2022-02-14 01:39:27 +0330

    created needed apps and deleted store pp

--- online_shop/online_shop/settings.py
+++ online_shop/online_shop/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'store',
+    'core',
+    'product',
+    'customer',
+    'order',
 ]
 
 MIDDLEWARE = [
