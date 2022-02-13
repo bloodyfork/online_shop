e22b5d6ac705555750c17f22c967f08cc9312448
bloodyfork <mohtor.138055@gmail.com>
2022-02-14 02:09:12 +0330

    created core model BaseManager

--- online_shop/customer/models.py
+++ online_shop/customer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class BaseManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
+    def archive(self):
+        return super().get_queryset()
