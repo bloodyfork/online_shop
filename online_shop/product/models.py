69478b0199c862388136d9f8b5e15843b60955d8
bloodyfork <mohtor.138055@gmail.com>
2022-02-14 03:06:00 +0330

    created model category

--- online_shop/product/models.py
+++ online_shop/product/models.py
@@ -14,3 +14,11 @@ class Product(BaseModel):
     in_stock = models.BooleanField(default=True)
     number_in_inventory = models.PositiveSmallIntegerField()
     discount = ...  #################################################################### ToDo FK --> Discount
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=20, unique=True, help_text="Enter name of category")
+    base_category = models.ForeignKey('self', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
