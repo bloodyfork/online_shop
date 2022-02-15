b528ae134c597a4b3daa4614c5f7df30bf619d82
bloodyfork <mohtor.138055@gmail.com>
2022-02-16 02:44:16 +0330

    created setup for Cart testcase

--- online_shop/customer/tests.py
+++ online_shop/customer/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
-
+from unittest import TestCase
+from django.contrib.auth.models import User
+from customer.models import Customer
 # Create your tests here.
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username="akbar", password="HeyYou1234")
+
+
+class CustomerTestCase(TestCase):
+    def setUp(self):
+        Customer.objects.create(user=User.objects.get(
+            username="akbar"),
+            customer__phone="09338616475")
--- online_shop/order/tests.py
+++ online_shop/order/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from unittest import TestCase
+from order.models import *
+from product.tests import Product
+from customer.tests import CustomerTestCase
+
 
 # Create your tests here.
+class OrderItemTestCase(TestCase):
+
+class CartTestCase(TestCase):
+    def setUp(self):
+        Cart.objects.create(customer=Customer.objects.get(id=1))
+
+    def test_total_price(self):
+        pass
+
+    def test_final_price(self):
+        pass
