ca4f1ffece8096516fa5ce85cbb82aab851ad7eb
bloodyfork <mohtor.138055@gmail.com>
2022-02-15 22:09:55 +0330

    created comment model

--- online_shop/product/models.py
+++ online_shop/product/models.py
@@ -6,12 +6,12 @@ from core.models import BaseModel
 
 class Product(BaseModel):
     name = models.CharField(max_length=25, unique=True, help_text="Enter name of the product ")
-    category = models.ForeignKey(to='Category', on_delete=models.CASCADE)
     brand = models.CharField(max_length=18, help_text="Enter brand of product")
-    image = models.ImageField(upload_to=..., help_text="Upload photo of product here")  # ToDo make upload work,SetMedia
-    price = models.PositiveIntegerField(help_text="Enter price of product")
+    category = models.ForeignKey(to='Category', on_delete=models.CASCADE)
     description = models.CharField(max_length=100, )
+    image = models.ImageField(upload_to=..., help_text="Upload photo of product here")  # ToDo make upload work,SetMedia
     in_stock = models.BooleanField(default=True)
+    price = models.PositiveIntegerField(help_text="Enter price of product")
     number_in_inventory = models.PositiveSmallIntegerField()
     discount = models.OneToOneField(to='Discount', on_delete=models.CASCADE)
 
@@ -32,3 +32,8 @@ class Discount(BaseModel):
 
     def __str__(self):
         return f"{self.value} {self.type}"
+
+
+class Comment(BaseModel):
+    context = models.CharField(max_length=120)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
