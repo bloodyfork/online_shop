cc37bc54fce0874572ca173ca41baae40ae3b7ee
bloodyfork <mohtor.138055@gmail.com>
2022-02-16 01:12:31 +0330

    created dis_actovator method in OffCode

--- online_shop/order/models.py
+++ online_shop/order/models.py
@@ -20,7 +20,7 @@ class Cart(BaseModel):
         all_order_items = self.orderitem_set.all()
         for order in all_order_items:
             each_price = order.product.price
-            price_for_all = each_price*order.how_many
+            price_for_all = each_price * order.how_many
             self.total_price += price_for_all
         return self.total_price
 
@@ -28,10 +28,10 @@ class Cart(BaseModel):
         all_order_items = self.orderitem_set.all()
         if self.off_code is not None:
             for order in all_order_items:
-                 discounted_prices = order.sum_of_prices_after_discount()
-                 self.final_price += discounted_prices
+                discounted_prices = order.sum_of_prices_after_discount()
+                self.final_price += discounted_prices
 
-            self.final_price = self.final_price*(100-self.off_code.value)//100
+            self.final_price = self.final_price * (100 - self.off_code.value) // 100
 
             return self.final_price
 
@@ -42,24 +42,29 @@ class Cart(BaseModel):
                 return self.final_price
 
 
-
-
-
 class OrderItem(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(to=Cart, on_delete=models.CASCADE)
     how_many = models.PositiveSmallIntegerField(help_text="How many of this product do you need")
 
     def sum_of_prices_after_discount(self):
-        res = self.product.after_discount_price()*self.how_many
+        res = self.product.after_discount_price() * self.how_many
         return res
 
 
 class OffCode(BaseModel):
     the_code = models.CharField(max_length=10, help_text="Enter code for offer", unique=True)
-    value = models.PositiveIntegerField(help_text="Enter code percentage for offer",validators=[MinValueValidator(1), MaxValueValidator(100)])
+    value = models.PositiveIntegerField(help_text="Enter code percentage for offer",
+                                        validators=[MinValueValidator(1), MaxValueValidator(100)])
     is_active = models.BooleanField(default=True)
     how_many_use = models.PositiveSmallIntegerField(default=3, help_text="how many times do you want this code")
+
+    def dis_activator(self):
+        if self.how_many_use == 0:
+            self.is_active = False
+            self.is_deleted = True
+        else:
+            pass
 
 # class status:
 #     title = models.CharField(choices=)
