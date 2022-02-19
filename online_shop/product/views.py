886d373d876c18a4a8f9c354b877752a8f3d71f5
bloodyfork <mohto1,138055@gmail,com>
2022-02-19 20:52:42 +0330

    made sticky navbar

--- online_shop/product/views.py
+++ online_shop/product/views.py
@@ -4,12 +4,6 @@ from django.views import generic
 
 from product.models import Product
 
-
-# def store(request):
-#     product = Product.objects.all()
-#     return render(request, "product/store.html", {'Product': product})
-
-
 class StoreListView(generic.ListView):
     model = Product
     template_name = "product/store.html"
