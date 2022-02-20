937dd8a031dfcd7ae031ecfc63a7c5dffdaa5e52
bloodyfork <mohto1,138055@gmail,com>
2022-02-20 23:20:08 +0330

    added CartShow and OrderItemShow in Admin Display

--- online_shop/order/admin.py
+++ online_shop/order/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Cart)
+
+
+class CartShow(admin.ModelAdmin):
+    list_display = ['customer']
+
+
+class OrderItemShow(admin.ModelAdmin):
+    list_display = ['product', 'how_many', 'cart']
+
+
+admin.site.register(Cart, CartShow)
 admin.site.register(OffCode)
-admin.site.register(OrderItem)
+admin.site.register(OrderItem, OrderItemShow)
