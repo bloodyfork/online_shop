de1332bfef0a1d17ae00ff2aaf19310b555076f5
bloodyfork <mohto1,138055@gmail,com>
2022-02-21 12:16:10 +0330

    planted register form in register.html

--- online_shop/customer/urls.py
+++ online_shop/customer/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 
+from customer.views import Login, Register
+
 urlpatterns = [
-    path('login/', Login.as_view(), name='login'),
+    # path('login/', Login.as_view(), name='login'),
     path('register/', Register.as_view(), name='register'),
 
 ]
--- online_shop/customer/views.py
+++ online_shop/customer/views.py
@@ -1,12 +1,14 @@
-
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 # Create your views here.
 from django.views import generic
 
 
-class Register(generic.):
+class Register(generic.FormView):
+    form_class = UserCreationForm
+    template_name = 'Customer/register.html'
+
+
+class Login:
     pass
-
-
-class Login():
