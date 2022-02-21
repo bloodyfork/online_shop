0053845c720c9ba2025862c6c9a5b1a8c2704b08
bloodyfork <mohto1,138055@gmail,com>
2022-02-21 20:36:45 +0330

    made messages for our registeration

--- online_shop/customer/views.py
+++ online_shop/customer/views.py
@@ -1,6 +1,6 @@
 # from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
-
+from django.contrib import messages
 from .forms import CreateUserForm
 # Create your views here.
 from django.views import generic
@@ -14,11 +14,17 @@ class Register(generic.FormView):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, "Account has been created with username" + username)
             return redirect(to='login')
+
+        else:
+            messages.warning(request, 'wrong input!')
+            return redirect(to='register')
 
 
 class Login(generic.FormView):
-    form_class = L
+    form_class = CreateUserForm
+    template_name = "Customer/login.html"
 
-# def Login(request):
-#     return render(request, 'Customer/login.html')
+
