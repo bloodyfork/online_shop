3b2cdebf0942e67dc204b99c1d663319ee742ba6
bloodyfork <mohto1,138055@gmail,com>
2022-02-25 14:29:42 +0330

    made the shoppint cart icon show number of items

--- online_shop/order/views.py
+++ online_shop/order/views.py
@@ -7,14 +7,11 @@ from order.models import Cart
 def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
-        # customer = Customer.objects.get_or_create(user=request.user)
         cart, created = Cart.objects.get_or_create(customer=customer, is_paid=False)
         items = cart.orderitem_set.all()
         context = {"items": items, "cart": cart}
 
-
     else:
-
         items = []
         context = {"items": items}
     return render(request, "order/cart.html", context)
