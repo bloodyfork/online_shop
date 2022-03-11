56d26ef28a9ee98da3406c48b3a3ee4f8e184164
bloodyfork <mohto1,138055@gmail,com>
2022-03-11 17:48:24 +0330

    finished Create for address

--- online_shop/customer/API.py
+++ online_shop/customer/API.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from rest_framework.generics import DestroyAPIView, UpdateAPIView, CreateAPIView
 from customer.models import Address
 from customer.serializers import AddressSerializer
@@ -19,5 +20,15 @@ class CreateAPIAddress(CreateAPIView):
     queryset = Address
 
     def create(self, request, *args, **kwargs):
-        print(request.data['zip'])
-
+        postal_code = request.data['zip']
+        province = request.data['province']
+        city = request.data['city']
+        exact_address = request.data['exact_address']
+        customer = request.user.customer
+        query = Address.objects.create(province=province, city=city,
+                                       exact_address=exact_address,
+                                       postal_code=postal_code,
+                                       customer=customer)
+        query.save()
+        # m = messages.info(request, 'Address has been successfully created')
+        return HttpResponse('m')
