711f2e384b5aa3049d5cbb684e7a7c91e4da1c91
bloodyfork <mohto1,138055@gmail,com>
2022-03-11 12:47:09 +0330

    fixed delete for address

--- online_shop/customer/API.py
+++ online_shop/customer/API.py
@@ -1,16 +1,10 @@
 from rest_framework.generics import DestroyAPIView
 from customer.models import Address
 from customer.serializers import AddressSerializer
+from order.models import OrderItem
 
 
 class DeleteAPIAddress(DestroyAPIView):
     serializer_class = AddressSerializer
+    queryset = Address
 
-    def get_queryset(self):
-
-        address_id = self.request.data['the_id']
-        query = Address.objects.get(id=address_id)
-        return query
-
-    def perform_destroy(self, instance):
-        self.get_queryset().delete()
