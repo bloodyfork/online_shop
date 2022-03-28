73ee34740bcd34b4ab970881d0a039c1a466f0f8
bloodyfork <mohto1,138055@gmail,com>
2022-03-28 14:05:11 +0430

    write CommentSerializer

--- online_shop/product/serializers.py
+++ online_shop/product/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from product.models import Category, Product
+from product.models import Category, Product, Comment
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -13,4 +13,10 @@ class CategorySerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = "__all__"
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
         fields = "__all__"
